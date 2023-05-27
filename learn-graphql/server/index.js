8eeee6ab134a3cc9e221e3c2eaf5e23d18d12264
KP824 <112226540+KP824@users.noreply.github.com>
2023-05-27 14:37:26 -0700

    Updated booksSchema file with graphql-tools/schema MakeExecuteableSchema

--- learn-graphql/server/index.js
+++ learn-graphql/server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const graphql = require('graphql');
+const fs = require('fs');
+const path = require('path');
+const { parse, print } = require('graphql');
 const { graphqlHTTP } = require('express-graphql');
 // schema for locally created data (books/authors)
-// const schema = require('./schema/booksSchema');
+const schema = require('./schema/booksSchema');
 // schema for star wars api
-const schema = require('./schema/starWarsSchema');
+// const schema = require('./schema/starWarsSchema');
 
 const app = express();
 
@@ -13,41 +16,82 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
+// declare a file path to write the logs to
+const filePath = path.join(__dirname, './logs', 'ast_logs.text');
+
+
 // Code from Jiecheng
 const printedSchema = graphql.printSchema(schema);
 // console.log(printedSchema);
 const schemaAST = graphql.parse(printedSchema);
-console.log('this is the parsedSchema:', schemaAST);
+// This console log as been moved to a separate file in the logs folder
+//console.log('this is the parsedSchema:', schemaAST);
 let fields = 0;
 let types = 0;
 let lists = 0;
 
-graphql.visit(schemaAST, {
-  [graphql.Kind.OBJECT_TYPE_DEFINITION]: {
-    enter(node){
-      console.log('this is the current type:', node.name.value);
-      types++;
+// Testing the use of print()
+// define a query (hardcode) to test
+const query = `
+  {
+    film {
+      title
+      release_date
+      characters {
+        name
+        species {
+          name
+        }
+      }
     }
-  },
-  [graphql.Kind.FIELD_DEFINITION]: {
-    enter(node){
-      console.log('this is the node:', node);
-      console.log('this is the current field:', node.name.value);
-      if(node.type.kind === 'ListType') lists++;
-      fields++;
-    }
-  },
-  [graphql.Kind.LIST]: {
-    enter(node){
-      console.log('this is the current list:', node.name.value);
-    }
+  }
+`;
+
+// // This parses the query into an object. To view, we can stringify it
+// const ast = parse(query);
+// console.log(`type: ${typeof ast}`);
+// console.log(`this is ast: ${JSON.stringify(ast)}`)
+// // This print method converts the ast information into a proper graphQL query
+// const astString = print(ast);
+// console.log(`this is astString: ${astString}`);
+
+// write the log files to be saved. The logMessage will be the parsed information from the query
+const logMessage = JSON.stringify(schemaAST);
+
+fs.writeFile(filePath, logMessage, (error) => {
+  if (error) {
+    console.log(`Error writing logs to file: `, error);
+  } else {
+    console.log(`Logs saved to: `, filePath);
   }
 });
 
-console.log('This is the number of fields:', fields);
-console.log('This is the number of types:', types);
-console.log('This is the number of lists:', lists);
 
+// graphql.visit(schemaAST, {
+//   [graphql.Kind.OBJECT_TYPE_DEFINITION]: {
+//     enter(node){
+//       console.log('this is the current type:', node.name.value);
+//       types++;
+//     }
+//   },
+//   [graphql.Kind.FIELD_DEFINITION]: {
+//     enter(node){
+//       console.log('this is the node:', node);
+//       console.log('this is the current field:', node.name.value);
+//       if(node.type.kind === 'ListType') lists++;
+//       fields++;
+//     }
+//   },
+//   [graphql.Kind.LIST]: {
+//     enter(node){
+//       console.log('this is the current list:', node.name.value);
+//     }
+//   }
+// });
+
+// console.log('This is the number of fields:', fields);
+// console.log('This is the number of types:', types);
+// console.log('This is the number of lists:', lists);
 
 
 const port = 8000
--- learn-graphql/server/schema/booksSchema.js
+++ learn-graphql/server/schema/booksSchema.js
@@ -3,6 +3,7 @@
 //This section had three goals: 1) Define schema, 2) Define relational data, 3) Define the root query
 const graphql = require('graphql');
 const _ = require('lodash');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const { 
   GraphQLObjectType, 
@@ -30,79 +31,79 @@ const authors = [
 ];
 
 
-// define type
-const AuthorType = new GraphQLObjectType({
-  name: 'Author',
-  // The main reason why we wrap these fields inside functions is so that when the page loads, all of the fields function definitions are already hoisted. When the proper query is called, then the query will be aware of which field and type to query through to get the right information. 
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    age: { type: GraphQLInt },
-    books: {
-      type: new GraphQLList(BookType),
-      resolve(parent, args) {
-        console.log(`this is parent for AuthorType -> books: ${JSON.stringify(parent)}`);
-        return _.filter(books, { authorId: parent.id })
-      }
-    }
-  })
-});
+// // define type
+// const AuthorType = new GraphQLObjectType({
+//   name: 'Author',
+//   // The main reason why we wrap these fields inside functions is so that when the page loads, all of the fields function definitions are already hoisted. When the proper query is called, then the query will be aware of which field and type to query through to get the right information. 
+//   fields: () => ({
+//     id: { type: GraphQLID },
+//     name: { type: GraphQLString },
+//     age: { type: GraphQLInt },
+//     books: {
+//       type: new GraphQLList(BookType),
+//       resolve(parent, args) {
+//         console.log(`this is parent for AuthorType -> books: ${JSON.stringify(parent)}`);
+//         return _.filter(books, { authorId: parent.id })
+//       }
+//     }
+//   })
+// });
 
-const BookType = new GraphQLObjectType({
-  name: 'Book',
-  fields: () => ({
-    id: { type: GraphQLID },
-    name: { type: GraphQLString },
-    genre: { type: GraphQLString },
-    author: {
-      type: AuthorType,
-      resolve(parent, args) {
-        console.log(`this is parent: ${JSON.stringify(parent)}`);
-        return _.find(authors, { id: parent.authorId });
-      }
-    }
-  })
-});
+// const BookType = new GraphQLObjectType({
+//   name: 'Book',
+//   fields: () => ({
+//     id: { type: GraphQLID },
+//     name: { type: GraphQLString },
+//     genre: { type: GraphQLString },
+//     author: {
+//       type: AuthorType,
+//       resolve(parent, args) {
+//         console.log(`this is parent: ${JSON.stringify(parent)}`);
+//         return _.find(authors, { id: parent.authorId });
+//       }
+//     }
+//   })
+// });
 
 
 
 // define root query. Any fields or types defined in this Root Query allows us to create complex queries on the front end side, as long as we define the relational points of the data. 
-const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
-  fields: {
-    // will start to construct multiple fields
-    book: {
-      type: BookType,
-      args: { id: { type: GraphQLID } }, // this is representative of id: '123' argument
-      resolve(parent, args) {
-        // code to get data from db/ other source
-        console.log(`What is typeof for args.id: ${typeof(args.id)}`); // When we make a query on the front end, this should log "string" in the terminal
-        return _.find(books, {id: args.id });
-      }
-    },
-    // define author field
-    author: {
-      type: AuthorType,
-      args: { id: { type: GraphQLID } },
-      resolve(parent, args) {
-        return _.find(authors, { id: args.id });
-      }
-    },
-    // setting up a list of books to query
-    books: {
-      type: new GraphQLList(BookType),
-      resolve(parent, args) {
-        return books
-      }
-    },
-    authors: {
-      type: new GraphQLList(AuthorType),
-      resolve(parent, args) {
-        return authors
-      }
-    }
-  }
-});
+// const RootQuery = new GraphQLObjectType({
+//   name: 'RootQueryType',
+//   fields: {
+//     // will start to construct multiple fields
+//     book: {
+//       type: BookType,
+//       args: { id: { type: GraphQLID } }, // this is representative of id: '123' argument
+//       resolve(parent, args) {
+//         // code to get data from db/ other source
+//         console.log(`What is typeof for args.id: ${typeof(args.id)}`); // When we make a query on the front end, this should log "string" in the terminal
+//         return _.find(books, {id: args.id });
+//       }
+//     },
+//     // define author field
+//     author: {
+//       type: AuthorType,
+//       args: { id: { type: GraphQLID } },
+//       resolve(parent, args) {
+//         return _.find(authors, { id: args.id });
+//       }
+//     },
+//     // setting up a list of books to query
+//     books: {
+//       type: new GraphQLList(BookType),
+//       resolve(parent, args) {
+//         return books
+//       }
+//     },
+//     authors: {
+//       type: new GraphQLList(AuthorType),
+//       resolve(parent, args) {
+//         return authors
+//       }
+//     }
+//   }
+// });
 
 /*
 Example query:
@@ -112,6 +113,50 @@ book (id: '123') {
 }
 */
 
-module.exports = new GraphQLSchema({
-  query: RootQuery
-});
+// module.exports = new GraphQLSchema({
+//   query: RootQuery
+// });
+
+const typeDefs = `
+  type Author {
+    id: Int!
+    name: String
+    age: Int
+    books: [Book]
+  }
+
+  type Book {
+    id: Int!
+    name: String
+    genre: String
+    author: [Author]
+  }
+  
+  type Query {
+    book(id: ID!): Book
+    author(id: ID!): Author
+    books: [Book]
+    authors: [Author]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    book: ({ id }) => books.find(book => book.id === id),
+    author: ({ id }) => authors.find(author => author.id === id),
+    books: (parent) => _.filter(books, { authorID: parent.id }),
+    authors: (parent) => _.filter(authors, { bookId: parent.id })
+  }
+};
+
+const testSchema = makeExecutableSchema({ typeDefs, resolvers });
+module.exports = testSchema;
+// this console logs the invocation of the resolver for book with the id of 1
+console.log(`This is the testSchema`, testSchema.getQueryType().getFields().book.resolve({ id: '1' }));
+
+// this log shows all the fields and their attributes within the schema
+console.log(`This is the testSchema`, testSchema.getQueryType().getFields());
+
+
+
+
