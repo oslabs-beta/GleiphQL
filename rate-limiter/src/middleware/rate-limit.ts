bf06b8ffe74733d05341b85234aa7ae51ceb7108
jiedong111 <jiedong111@gmail.com>
2023-06-03 13:59:45 -0400

    Query traversal using visit function, added throttling based on complexity for queries

--- rate-limiter/src/middleware/rate-limit.ts
+++ rate-limiter/src/middleware/rate-limit.ts
@@ -1,70 +1,122 @@
 import { Request, Response, NextFunction } from 'express';
-import { parse, GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import { isAbstractType, GraphQLInterfaceType, isNonNullType, GraphQLType, GraphQLField, parse, GraphQLList, GraphQLObjectType, GraphQLSchema, TypeInfo, visit, visitWithTypeInfo, StringValueNode, getNamedType, GraphQLNamedType, getEnterLeaveForKind, GraphQLCompositeType, getNullableType, Kind, isListType, DocumentNode } from 'graphql';
+import { print } from 'graphql/language/printer';
 
-const rateLimiter = function (schema: GraphQLSchema, config: any) {
+const rateLimiter = function (complexityLimit: number, listLimit: number, schema: GraphQLSchema, schemaTypeInfo: TypeInfo, config: any) {
   return (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.query) {
-      const ast = parse(req.body.query);
-      let rootType = schema.getQueryType();
-      let typeScore = 0;
+    if(req.body.query) {
 
-      function findArgs(argName: string, argValue: string): number {
-        const validArgs = ['limit', 'first', 'last']
-        if (validArgs.includes(argName)) {
-          return Number(argValue)
-        }
-        else {
-          return 0
-        }
-      }
-
-      function calculateTypeCost(node: any, parentType: any, multiplier: number = 1) {
-        if (node.selectionSet) {
-          for (let i = 0; i < node.selectionSet.selections.length; i++) {
-            const nodeName = node.selectionSet.selections[i].name.value;
-
-            // conditional statement to check if query is an "introspection" query
-            if (nodeName !== '__schema') {
-
-              // conditional statement to check if the node type is a GraphQLList
-              if (parentType.getFields()[nodeName].type instanceof GraphQLList) {
-                const childType = parentType.getFields()[nodeName].type.ofType
-                typeScore += multiplier
-                console.log(`Type cost after adding GraphQLLIST type ${nodeName}:`, typeScore)
-                if (node.selectionSet.selections[i].arguments.length) {
-                  const argName = node.selectionSet.selections[i].arguments[0].name.value
-                  const argValue = node.selectionSet.selections[i].arguments[0].value.value
-                  const childMultiplier = multiplier * findArgs(argName, argValue);
-                  calculateTypeCost(node.selectionSet.selections[i], childType, childMultiplier);
-                } 
-                else {
-                  calculateTypeCost(node.selectionSet.selections[i], childType, multiplier)
-                }
-  
-              }
-              // conditional statement to check if the node type is a GraphQLObject
-              else if (parentType.getFields()[nodeName].type instanceof GraphQLObjectType) {
-                const childType = parentType.getFields()[nodeName].type
-                typeScore += multiplier
-                console.log(`Type cost after adding GraphQLObject type ${nodeName}:`, typeScore)
-                if (node.selectionSet.selections[i].arguments.length) {
-                  const argName = node.selectionSet.selections[i].arguments[0].name.value;
-                  const argValue = node.selectionSet.selections[i].arguments[0].value.value;
-                  const childMultiplier = multiplier * findArgs(argName, argValue);
-                  calculateTypeCost(node.selectionSet.selections[i], childType, childMultiplier);
-                }
-                else {
-                  calculateTypeCost(node.selectionSet.selections[i], childType, multiplier);
-                }
+      //function parses passed in AST assigns given listLimit to any limit argument nodes encountered
+      //unclear how useful this functionality actually is, but interesting to note that it is possible to parse and reconstruct a query AST
+      //presumably the endpoint would have configured some sort of list pagination/heuristics on their end
+      //unclear whether providing this throttle function would be overly useful in the library itself
+      const throttle = function(ast: DocumentNode, listLimit: number) {
+        let newAst = visit(ast, {
+          //@ts-ignore
+          Argument(node) {
+            if (node.name.value === 'limit' && node.value.kind === 'IntValue') {
+              let argValue = parseInt(node.value.value, 10);
+              console.log(`Argvalue = ${argValue}`);
+              if (argValue > listLimit) {
+                console.log(`ArgValue has exceeded passed in limit, modifying`)
+                return {
+                  ...node,
+                  value: {
+                    ...node.value,
+                    value: listLimit.toString(),
+                  },
+                };
               }
             }
+          },
+        });
+        req.body.query = print(newAst);
+        return newAst;
+      }
+
+      const parsedAst = parse(req.body.query);
+      const throttledAst = throttle(parsedAst, listLimit);
+      let parentTypeStack: any[]= [];
+      let complexityScore = 0;
+      let currMult = 0;
+
+      visit(throttledAst, visitWithTypeInfo(schemaTypeInfo, {
+        enter(node) {
+
+          //upon entering field, updates the parentTypeStack, which is the stack of all field ancestors of current field node
+          if (node.kind === Kind.FIELD) {
+
+            //resets list multiplier upon encountering an empty stack, indicating that a set of nested fields has been exited
+            if(parentTypeStack.length === 0) currMult = 0;
+
+            const parentType = schemaTypeInfo.getParentType();
+            if (parentType) {
+            const fieldDef = parentType && isAbstractType(parentType) ? schemaTypeInfo.getFieldDef() : parentType.getFields()[node.name.value];
+
+            if(fieldDef) {
+            const fieldDefArgs = fieldDef.args;
+            console.log('These are the fieldArgs:', fieldDef.args);
+            const fieldType = getNullableType(fieldDef.type);
+            const isList = isListType(fieldType) || (isNonNullType(fieldType) && isListType(fieldType.ofType));
+
+            console.log('This is the currentNode:', node.name.value);
+            console.log(parentTypeStack);
+            // let currMult = 0;
+            //functionality upon encountering listType, in short looks for ancestor lists and limit arguments then adjust currMult accordingly
+            if(isList === true) {
+              console.log(`${node.name.value} is a list`);
+              const argNode = node.arguments?.find(arg => arg.name.value === 'limit');
+              currMult = listLimit;
+              if (argNode && argNode.value.kind === 'IntValue') {
+                const argValue = parseInt(argNode.value.value, 10);
+                console.log(`Found limit argument with value ${argValue}`);
+                if(argValue < listLimit) currMult = argValue;
+                // console.log('Mult is now:', currMult);
+              }
+              console.log('Mult is now:', currMult);
+              for (let i  =0; i < parentTypeStack.length; i++) {
+                if(parentTypeStack[i].isList === true) currMult = currMult * parentTypeStack[i].currMult
+              }
+              complexityScore += currMult;
+
+              //handling for object types, checks for most recent ancestor list's multiplier then adjust accordingly
+            } else if (fieldType instanceof GraphQLObjectType) {
+              console.log(`This is the parentStack of the graphQLObject type ${node.name.value}`, parentTypeStack);
+              for (let i = parentTypeStack.length-1; i >= 0; i--) {
+                if(parentTypeStack[i].isList === true) {
+                  currMult = parentTypeStack[i].currMult
+                  break;
+                }
+              }
+
+              //if the currMult === 0 indicates object not nested in list, simply increment complexity score
+              if(currMult !== 0) complexityScore += currMult; else complexityScore++;
+            }
+            // console.log('This is the nested list count:', nestedListCount);
+
+            parentTypeStack.push({fieldDef, isList, fieldDefArgs, currMult});
+
           }
         }
+        }
+        },
+        leave(node) {
+          if (node.kind === Kind.FIELD) {
+            parentTypeStack.pop();
+          }
+          console.log('Exiting node')
+        }
+      }));
+
+      console.log('This is the complexity score:', complexityScore);
+
+      //returns error if complexity heuristic reads complexity score over limit
+      if(complexityScore >= complexityLimit) {
+        console.log('Complexity of this query is too high');
+        return next(Error);
       }
-      calculateTypeCost(ast.definitions[0], rootType)
-      console.log('Total type cost: ', typeScore)
-    }
-    return next();
-  };
 };
+return next();
+  }
+}
 export default rateLimiter;
--- rate-limiter/src/query-test/query-test.ts
+++ rate-limiter/src/query-test/query-test.ts
@@ -6,21 +6,28 @@ import { UrlLoader } from '@graphql-tools/url-loader';
 import { createYoga } from 'graphql-yoga'
 import endpointMonitor from '../middleware/monitoring.js';
 import rateLimiter from '../middleware/rate-limit.js';
+import { TypeInfo } from 'graphql'
 
 const app = express();
 
 //loadSchema can be any public graphql endpoint
 const publicSchema = await loadSchema('https://spacex-production.up.railway.app/', { loaders: [new UrlLoader()]})
 
+const schemaTypeInfo = new TypeInfo(publicSchema);
+
 const yoga = createYoga({
   schema: publicSchema,
   graphiql: true
 })
 
+const limit = 200;
+
+const listLimit = 10;
+
 app.use(express.json());
 
-app.use('/graphql', rateLimiter(publicSchema, {testConfig: 'testConfig'}), endpointMonitor, yoga)
+app.use('/graphql', rateLimiter(limit, listLimit, publicSchema, schemaTypeInfo, {testConfig: 'testConfig'}), endpointMonitor, yoga)
 
 app.listen(4000, () => {
-  console.info('Server is running on http://localhost:4000/graphql')
+  console.info("Server is running on http://localhost:4000/graphql")
 })
