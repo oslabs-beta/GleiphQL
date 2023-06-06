975dbc4b2c000c58acb92242d08c2333aa26c014
Yeong SilYoon <wendyys96@ucla.edu>
2023-06-05 19:26:24 -0700

    merged dev to yyoon/webAppBE

--- web-app/TS_src/client/main.tsx
+++ web-app/TS_src/client/main.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import App from './App';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './pages/error-page.js';
+
+// this placeholder for only router we need in whole application
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+])
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
