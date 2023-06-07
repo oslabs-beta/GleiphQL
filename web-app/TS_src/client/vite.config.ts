85916aacd74c6c9a3f7e44341840ee7779d96a79
Yeong SilYoon <wendyys96@ucla.edu>
2023-06-07 00:44:26 -0700

    completed implementing basic functionalities for dataController, endpointContoller, dataRouter, and endpointRouter. merged loginRouter and registerRouter to accountRouter

--- web-app/TS_src/client/vite.config.ts
+++ web-app/TS_src/client/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-		port: 3003,
+		port: 3000,
 		proxy: {
 			'/api': 'http://localhost:3500'
 		}
