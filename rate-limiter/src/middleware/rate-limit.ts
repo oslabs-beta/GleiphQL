6cb580f747e98c194e87ff12dfc6d6497cdd52b2
jiedong111 <jiedong111@gmail.com>
2023-06-27 21:38:43 -0400

    Verified extraction of cost directive and application to relevant fields

--- rate-limiter/src/middleware/rate-limit.ts
+++ rate-limiter/src/middleware/rate-limit.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
-import { isAbstractType, GraphQLInterfaceType, isNonNullType, GraphQLType, GraphQLField, parse, GraphQLList, GraphQLObjectType, GraphQLSchema, TypeInfo, visit, visitWithTypeInfo, StringValueNode, getNamedType, GraphQLNamedType, getEnterLeaveForKind, GraphQLCompositeType, getNullableType, Kind, isListType, DocumentNode } from 'graphql';
+import { buildSchema, isAbstractType, GraphQLInterfaceType, isNonNullType, GraphQLType, GraphQLField, parse, GraphQLList, GraphQLObjectType, GraphQLSchema, TypeInfo, visit, visitWithTypeInfo, StringValueNode, getNamedType, GraphQLNamedType, getEnterLeaveForKind, GraphQLCompositeType, getNullableType, Kind, isListType, DocumentNode } from 'graphql';
 import graphql from 'graphql';
 import { useSchema } from 'graphql-yoga';
+import { buildResolveInfo } from 'graphql/execution/execute';
 
 //to-dos
 //modularize code, certain functions can be offloaded
@@ -13,29 +14,108 @@ const rateLimiter = function (config: any) {
   return (req: Request, res: Response, next: NextFunction) => {
     if(req.body.query) {
 
-      const parsedAst = parse(req.body.query);
+      // const schemaType = new TypeInfo(config.schema);
+      // const parsedAst = parse(req.body.query);
+
+      const testSDL = `
+
+directive @cost(value: Int) on FIELD_DEFINITION
+
+type Author {
+  id: ID! @cost(value: 1)
+  name: String @cost(value: 2)
+  books: [Book] @cost(value: 3)
+}
+
+type Book {
+  id: ID! @cost(value: 1)
+  title: String @cost(value: 2)
+  author: Author @cost(value: 3)
+}
+
+type Query {
+  authors: [Author] @cost(value: 2)
+  books: [Book] @cost(value: 2)
+}
+      `
+
+      const testQuery = `
+      query {
+        authors {
+          id
+          name
+          books {
+            title
+          }
+        }
+        books {
+          id
+          title
+          author {
+            name
+          }
+        }
+      }
+      `
+
+      const builtSchema = buildSchema(testSDL)
+      const schemaType = new TypeInfo(builtSchema);
+      const parsedAst = parse(testQuery);
       let parentTypeStack: any[]= [];
       let complexityScore = 0;
       let typeComplexity = 0;
       let resolveComplexity = 0;
       let currMult = 0;
 
-      visit(parsedAst, visitWithTypeInfo(config.typeInfo, {
+      visit(parsedAst, visitWithTypeInfo(schemaType, {
         enter(node) {
 
           //upon entering field, updates the parentTypeStack, which is the stack of all field ancestors of current field node
           if (node.kind === Kind.FIELD) {
 
+            let baseVal = 1;
             //resets list multiplier upon encountering an empty stack, indicating that a set of nested fields has been exited
             if(parentTypeStack.length === 0) currMult = 0;
 
-            const parentType = config.typeInfo.getParentType();
+            const parentType = schemaType.getParentType();
             if (parentType) {
-            const fieldDef = parentType && isAbstractType(parentType) ? config.typeInfo.getFieldDef() : parentType.getFields()[node.name.value];
+            const fieldDef = parentType && isAbstractType(parentType) ? schemaType.getFieldDef() : parentType.getFields()[node.name.value];
 
             if(fieldDef) {
+            console.log('this is currNode', node.name.value);
+            console.log('directives?', fieldDef.astNode?.directives);
             const fieldDefArgs = fieldDef.args;
             console.log('These are the fieldArgs:', fieldDef.args);
+            if(fieldDef.astNode) {
+              if(fieldDef.astNode.directives) {
+                console.log('This is the current value of baseVal', baseVal)
+                const directives: any[] = [];
+                for (let i = 0; i < fieldDef.astNode.directives.length; i++) {
+                  console.log('These are the directives', fieldDef.astNode.directives[i]);
+                  directives.push({name: fieldDef.astNode.directives[i].name, arguments: fieldDef.astNode.directives[i].arguments})
+                  console.log('These are the pushed directives', directives[i]);
+                }
+                if(directives.length){
+                  for (let i = 0; i < directives.length; i++) {
+                    const map = directives[i].arguments?.map((arg: any) => ({
+                      name: directives[i].name.value,
+                      value: arg.value
+                    }))
+
+                    console.log('This is the map', map)
+
+                    const cost = map.find((ele: any) => {
+                      if(ele.name === 'cost' && ele.value){
+                        baseVal = ele.value.value;
+                      }
+                    })
+                  }
+                }
+                console.log('This is the value of baseVal after accounting for directives', baseVal)
+              }
+            }
+
+
             const fieldType = getNullableType(fieldDef.type);
             // const fieldDirectives = fieldDef.astNode.directives;
             // if(fieldDirectives) {
@@ -69,7 +149,7 @@ const rateLimiter = function (config: any) {
 
               //base case addition of resolveComplexity, offset in above for-loop for list cases
               resolveComplexity++;
-              typeComplexity += currMult;
+              typeComplexity += currMult * baseVal;
 
               //handling for object types, checks for most recent ancestor list's multiplier then adjust accordingly
             } else if (fieldType instanceof GraphQLObjectType) {
@@ -84,7 +164,7 @@ const rateLimiter = function (config: any) {
               }
 
               //if the currMult === 0 indicates object not nested in list, simply increment complexity score
-              if(currMult !== 0) typeComplexity += currMult; else typeComplexity++;
+              if(currMult !== 0) typeComplexity += currMult * baseVal; else typeComplexity+= baseVal;
               resolveComplexity++;
             }
             parentTypeStack.push({fieldDef, isList, fieldDefArgs, currMult});
