16474e7aadb82e8f8626b972d3998ff02f61eb98
KP824 <112226540+KP824@users.noreply.github.com>
2023-07-20 16:32:17 -0700

    Basic functionality for engineer modals are done. Will need to add custom styling to the header

--- web-app/TS_src/client/components/MeetTheTeam.tsx
+++ web-app/TS_src/client/components/MeetTheTeam.tsx
@@ -5,39 +5,49 @@ const MeetTheTeam: React.FC<{}> = () => {
 
   return (
     <>
-      <div className=''>
-        <h3>Meet the Team</h3>
+      <div className='meetTheTeam-bg flex flex-col justify-center items-center p-10'>
+        <h1>Meet the Team</h1>
+
+        <div className='flex flex-wrap justify-center'>
+          <ProfileCard
+            imageSrc = '/images/JDong.png'
+            memberName = 'Jiecheng Dong'
+            githubLink = 'https://github.com/jiedong111'
+            linkedinLink = 'https://www.linkedin.com/in/jiecheng-dong-1522b8248/'
+            memberBio={`A part-time artist whose drawings rival that of Leonardo da Vinci. With an eloquent vocabulary that flows like an ocean, this engineer's linguistic prowess and artistic depth will undoubtedly leave you awe-inspired!`}
+          />
+
+          <ProfileCard
+            imageSrc='/images/ALarkin.jpg'
+            memberName='Andrew Larkin'
+            githubLink='https://github.com/larkinaj'
+            linkedinLink='https://www.linkedin.com/in/andrew-larkin-71395940/'
+            memberBio={`Meet our Bolivian hot shot, the technical support maestro with an army of skills! With 6 years of service under his belt, he's a full-stack mogul who can fix any glitch and conquer any coding challenge with a smile that could charm even the toughest bugs!`}
+          />
+
+          <ProfileCard
+            imageSrc='/images/KPhan.jpg'
+            memberName='Kevin Phan'
+            githubLink='https://github.com/KP824'
+            linkedinLink='https://www.linkedin.com/in/kevinphan760/'
+            memberBio={`bruh`}
+          />
+
+          <ProfileCard
+            imageSrc='/images/YYoon.jpg'
+            memberName='Yeong Sil Yoon'
+            githubLink='https://github.com/wendyys96'
+            linkedinLink=''
+            memberBio={`Introducing the unstoppable force of tech and tunes! 🎤🔧 Our 24/7 hustler doubles as a Kpop idol, spreading smiles and beats worldwide. But don't be fooled by the dazzling stage presence—this fierce full-stack engineer has an affinity for backend problem-solving skills that'll leave even the toughest bugs shaking in their virtual boots!`}
+          />
+        </div>
+
       </div>
       
-      <div className='flex flex-wrap'>
-        <ProfileCard
-          imageSrc = '/images/JDong.png'
-          memberName = 'Jiecheng Dong'
-          githubLink = 'https://github.com/jiedong111'
-          linkedinLink = 'https://www.linkedin.com/in/jiecheng-dong-1522b8248/'
-        />
-
-        <ProfileCard
-          imageSrc='/images/ALarkin.jpg'
-          memberName='Andrew Larkin'
-          githubLink='https://github.com/larkinaj'
-          linkedinLink='https://www.linkedin.com/in/andrew-larkin-71395940/'
-        />
-
-        <ProfileCard
-          imageSrc='/images/KPhan.jpg'
-          memberName='Kevin Phan'
-          githubLink='https://github.com/KP824'
-          linkedinLink='https://www.linkedin.com/in/kevinphan760/'
-        />
-
-        <ProfileCard
-          imageSrc='/images/YYoon.jpg'
-          memberName='Yeong Sil Yoon'
-          githubLink='https://github.com/wendyys96'
-          linkedinLink=''
-        />
-      </div>
+        
+      
+      
+      
     </>
   );
 };
