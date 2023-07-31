c5ad02802e71e3a31ea42284597b2c257c2cdd0e
KP824 <112226540+KP824@users.noreply.github.com>
2023-07-31 16:57:26 -0700

    Testing functionality for userAuth is completed. Next is to complete splash page testing and then dashbaord

--- web-app/cypress/e2e/userAuth_spec.cy.ts
+++ web-app/cypress/e2e/userAuth_spec.cy.ts
@@ -1,41 +1,27 @@
 // helper function to check if test email exists and delete it
-const checkAndDeleteTestUser = () => {
+const deleteTestUser = () => {
   cy.request({
     method: 'POST',
-    url: 'http://localhost:3000/api/account/login',
+    url: 'http://localhost:3000/api/account/logout',
     body: {
-      'username': 'testEmail1.gmail.com',
+      'email': 'testEmail1@gmail.com',
       'password': 'test1',
     },
-  }  ).then((response) => {
-      if (response.status === 200) {
-        // User exists, so delete it
-        cy.request({
-          method: 'POST',
-          url: 'http://localhost:3000/api/account/logout',
-          body: {
-            'username': 'testEmail1.gmail.com',
-            'password': 'test1',
-          },
-        }).then(() => {
-          cy.request({
-            method: 'DELETE',
-            url: 'http://localhost:3000/api/account/deleteUser',
-            body: {
-              'username': 'testEmail1.gmail.com',
-              'password': 'test1',
-            }
-          });
-        });
-      }
-    });
+  });
+  
+  cy.request({
+    method: 'DELETE',
+    url: 'http://localhost:3000/api/account/deleteUser',
+    body: {
+      'email': 'testEmail1@gmail.com',
+      'password': 'test1',
+    }
+  });
 }
-
 
 describe(`Testing functionality of register component`, () => {
   // Navigate to the register component
   beforeEach(() => {
-    checkAndDeleteTestUser();
     cy.visit('http://localhost:3000')
     cy.get('[data-cy=login-btn]').click()
     cy.get('[data-cy=toggle-register]').click()
@@ -54,7 +40,7 @@ describe(`Testing functionality of register component`, () => {
 
   it('Successfully creating a new user', () => {
     // input test data to create user
-    cy.get('[data-cy=register-username]').type(`testEmail1.gmail.com`)
+    cy.get('[data-cy=register-username]').type(`testEmail1@gmail.com`)
     cy.get('[data-cy=register-password').type(`test1`)
     cy.get('[data-cy=register-confirm-password').type(`test1`)
 
@@ -68,69 +54,76 @@ describe(`Testing functionality of register component`, () => {
     cy.url().should('include', '/')
   })
 
-  // it('Failed creating a new user', () => {
-  //   // input test data to create user
-  //   cy.get('[data-cy=register-username]').type(`Testbot8`)
-  //   cy.get('[data-cy=register-password').type(`888`)
-  //   cy.get('[data-cy=register-confirm-password').type(`888`)
+  // delete test user after successful creation
+  after(() => {
+    deleteTestUser();
+  })
 
-  //   // click complete register
-  //   cy.get('[data-cy=register-submit]').click()
+  it('Failed creating a new user', () => {
+    // input test data to create user
+    cy.get('[data-cy=register-username]').type(`Testbot8`)
+    cy.get('[data-cy=register-password').type(`888`)
+    cy.get('[data-cy=register-confirm-password').type(`888`)
 
-  //   // failed login has a pop up
-  //   cy.get('.Toastify__toast-container')
-  //     .should('be.visible')
-  //     .contains('Could not create account. Try again.')
+    // click complete register
+    cy.get('[data-cy=register-submit]').click()
 
-  //   cy.url().should('include', '/')
-  // })
+    // failed login has a pop up
+    cy.get('.Toastify__toast-container')
+      .should('be.visible')
+      .contains('Could not create account. Try again.')
+
+    cy.url().should('include', '/')
+  })
+
+  
 })
 
 
-// describe ('Testing functionality of login component', () => {
+describe ('Testing functionality of login component', () => {
 
-//   // Navigate to the login component
-//   beforeEach(() => {
-//     cy.visit('http://localhost:3000')
-//     cy.get('[data-cy=login-btn]').click()
-//   })
+  // Navigate to the login component
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+    cy.get('[data-cy=login-btn]').click()
+  })
 
-//   it ('Testing text content of login component', () => {
-//     cy.get('.text-sky-900')
-//       .should('have.text', 'SIGN IN')
+  it ('Testing text content of login component', () => {
+    cy.get('.text-sky-900')
+      .should('have.text', 'SIGN IN')
 
-//     cy.get('.w-full > .justify-center')
-//       .should('have.text', 'Not a member? Sign up here!')
-//   })
+    cy.get('.w-full > .justify-center')
+      .should('have.text', 'Not a member? Sign up here!')
+  })
 
-//   it ('Testing successful login functionality', () => {
-//     // input test data to login
-//     cy.get('[data-cy=login-username]').type(`${testUsername}`);
-//     cy.get('[data-cy=login-password]').type(`${testPassword}`);
+  it ('Testing successful login functionality', () => {
+    // input test data to login
+    cy.get('[data-cy=login-username]').type('Testbot7');
+    cy.get('[data-cy=login-password]').type('777');
 
-//     // click the login button
-//     cy.get('[data-cy=login-submit]').click()
+    // click the login button
+    cy.get('[data-cy=login-submit]').click()
 
-//     // successful login has a pop up
-//     cy.get('.Toastify__toast-container')
-//       .should('be.visible')
-//       .contains('Login successful!')
+    // successful login has a pop up
+    cy.get('.Toastify__toast-container')
+      .should('be.visible')
+      .contains('Login successful!')
 
-//     // redirects to dashboard
-//     cy.url().should('include', '/dashboard');
-//   })
+    // redirects to dashboard
+    cy.url().should('include', '/dashboard');
+  })
 
-//   it ('Testing failed login functionality', () => {
-//     // input incorrect data to login
-//     cy.get('[data-cy=login-username]').type('failedTest@fail.org');
-//     cy.get('[data-cy=login-password').type('noSuchAccount');
-//     // submit
-//     cy.get('[data-cy=login-submit]').click()
+  it ('Testing failed login functionality', () => {
+    // input incorrect data to login
+    cy.get('[data-cy=login-username]').type('failedTest@fail.org');
+    cy.get('[data-cy=login-password').type('noSuchAccount');
+    // submit
+    cy.get('[data-cy=login-submit]').click()
 
-//     // failed pop up window
-//     cy.get('.Toastify__toast-container')
-//       .should('be.visible')
-//       .contains('Login unsuccessful. Try again.')
-//   });
+    // failed pop up window
+    cy.get('.Toastify__toast-container')
+      .should('be.visible')
+      .contains('Login unsuccessful. Try again.')
+  });
 
-// });
+});
