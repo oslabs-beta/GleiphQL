6cdab72921d002a7bbf0b6b5fecbdbca5d7535d1
Yeong SilYoon <wendyys96@ucla.edu>
2023-07-31 23:52:42 -0700

    commiting the changes in sidebar

--- web-app/TS_src/client/components/Sidebar.tsx
+++ web-app/TS_src/client/components/Sidebar.tsx
@@ -89,7 +89,7 @@ const Sidebar: FC = () : ReactElement => {
   return (
       <nav
         id='sidenav-8'
-        className={menuCollapsed? 'opacity-30 sm:opacity-100' : 'opacity-100' + 'left-0 z-[1035] h-full w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden="false"]:translate-x-0 dark:bg-zinc-800'}
+        className={ (menuCollapsed? 'opacity-30 sm:opacity-100' : 'opacity-100') + 'left-0 z-[1035] h-full w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden="false"]:translate-x-0 dark:bg-zinc-800'}
         data-te-sidenav-init
         data-te-sidenav-hidden='false'
         data-te-sidenav-mode='side'
